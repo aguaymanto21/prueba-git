b83d2e14cbd091c4426a45a163cd9b360cb5544f
gslazar96@gmail.com <gslazar96@gmail.com>
2024-10-05 16:39:07 -0500

    Paginacion

--- js/script.js
+++ js/script.js
@@ -1,31 +1,36 @@
 $(document).ready(function() {
-  // Número de cards por página
-  var itemsPerPage = 3;
+  // Número de tarjetas por página
+  var itemsPerPage = 3; // Cambia este valor según tus necesidades
 
-  // Selecciona todos los elementos de card
-  var items = $('.custom-card');
+  // Selecciona todos los elementos de tarjeta
+  var items = $('.custom-skill-item');
   var numItems = items.length;
 
-  // Oculta todos los items inicialmente
+  // Oculta todas las tarjetas inicialmente
   items.hide();
 
-  // Inicializa la paginación
+  // Inicializa la paginación sin los botones "Anterior" y "Siguiente"
   $('#pagination-container').pagination({
-      items: numItems,
-      itemsOnPage: itemsPerPage,
-      cssStyle: 'light-theme',
-      prevText: 'Anterior',
-      nextText: 'Siguiente',
-      onPageClick: function(pageNumber) {
-          // Calcula los índices inicial y final de los items a mostrar
-          var showFrom = (pageNumber - 1) * itemsPerPage;
-          var showTo = showFrom + itemsPerPage;
+    items: numItems,
+    itemsOnPage: itemsPerPage,
+    cssStyle: '',
+    // Removemos el texto de "Anterior" y "Siguiente" estableciendo las cadenas vacías
+    prevText: '',
+    nextText: '',
+    // Opciones para ocultar los botones "Anterior" y "Siguiente"
+    useAnchors: false,
+    displayedPages: 2, // Número de páginas a mostrar (en tu caso, 2)
+    edges: 0, // Número de páginas de borde (al principio y al final)
+    onPageClick: function(pageNumber) {
+      // Calcula los índices inicial y final de las tarjetas a mostrar
+      var showFrom = (pageNumber - 1) * itemsPerPage;
+      var showTo = showFrom + itemsPerPage;
 
-          // Oculta todos los items y muestra solo los correspondientes a la página actual
-          items.hide().slice(showFrom, showTo).show();
-      }
+      // Oculta todas las tarjetas y muestra solo las correspondientes a la página actual
+      items.hide().slice(showFrom, showTo).show();
+    }
   });
 
-  // Muestra los primeros items al cargar la página
+  // Muestra las primeras tarjetas al cargar la página
   items.slice(0, itemsPerPage).show();
 });
